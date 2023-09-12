d1257296587e5279b23258d9fe291f57e346c64e
doozie-Muralidharan <muralidharanr@dooziesoft.com>
2023-09-12 17:14:57 +0530

    customers table fix

--- database/migrations/2023_09_12_113411_drop_status_from_customers_table.php
+++ database/migrations/2023_09_12_113411_drop_status_from_customers_table.php
@@ -26,7 +26,7 @@ class DropStatusFromCustomersTable extends Migration
     public function down()
     {
         Schema::table('customers', function (Blueprint $table) {
-            //
+            $table->dropColumn('status');
         });
     }
 }
